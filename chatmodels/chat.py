22ca9bfb6dc26f94e9ac9b696c48c534708ad350
hunesh kumar <huneshk@gmail.com>
2026-03-03 12:09:29 +0500

    gimni invoke call

---
+++ chatmodels/chat.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+from langchain.chat_models import init_chat_model
+
+load_dotenv()  # Load environment variables from .env file
+
+model= init_chat_model("google_genai:gemini-2.5-flash-lite", temperature=0.7)
+
+response = model.invoke("What is the capital of France?")
+print(response.content)
